0b72873f6a4db7cea5e82cafacefc9d3e3d7a688
Arrafi2644 <raffian2644@gmail.com>
2025-06-22 10:55:29 +0600

    Add get all books with filter limit ort

--- src/app/controllers/books.controllers.ts
+++ src/app/controllers/books.controllers.ts
@@ -4,11 +4,12 @@ import { Book } from '../models/books.model';
 
 const booksRoutes = express.Router();
 
+// create book 
+
 booksRoutes.post('/', async (req: Request, res: Response) => {
 
     try {
         const bookBody = req.body;
-        console.log(req.body, "req.body theke ");
 
         const book = await Book.create(bookBody);
 
@@ -18,18 +19,41 @@ booksRoutes.post('/', async (req: Request, res: Response) => {
             data: book
         })
 
-        console.log("book body ", bookBody);
-        console.log("book created  ", book);
     } catch (error: any) {
-     res.status(201).json({
+        res.status(500).json({
             success: false,
             message: error.message,
             error: error
         })
     }
 
+})
 
+// get all books 
+booksRoutes.get('/', async (req: Request, res: Response) => {
+    try {
+        const filter = req.query.filter as string || "";
+        const sortBy = req.query.sortBy as string || "createdAT"
+        const sort = req.query.sort === "asc" ? 1 : -1;
+        const limit = parseInt(req.query.limit as string) || 10;
+        const query = { genre: filter }
+        const books = await Book.find(query)
+            .sort({ [sortBy]: sort })
+            .limit(limit)
 
+        res.status(200).json({
+            success: true,
+            message: "Books retrieved successfully",
+            data: books
+        })
+
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error
+        })
+    }
 })
 
 export default booksRoutes;

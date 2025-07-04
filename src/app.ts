0aa7a657b7eeecda490d1cc4272f3f7ea85a9548
Arrafi2644 <raffian2644@gmail.com>
2025-07-04 11:46:19 +0600

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express, {Application, Request, Response} from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors"
 const app: Application = express();
 
 app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5173"],
+}))
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Library management app is running!")
